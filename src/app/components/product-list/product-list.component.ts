3f0037300c065a5af69583d6fc62a4ecbe3311d2
ike <ikeikeike1024@gmail.com>
2020-03-08 11:30:20 +0800

    Product list page perfect, css decoration, logic writing

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -20,5 +20,69 @@ export class ProductListComponent implements OnInit {
       this.base = ''
      }
 
+  switchBase(base) {
+    for(let i = 0; i < this.products.length; i ++) {
+      if (this.products[i].price.base != base) {
+        for (let j = 0; j < this.exchangeRates.length; j ++) {
+          if (this.products[i].price.base == this.exchangeRates[j].base) {
+            this.products[i].price.base = base
+            this.products[i].price.amount = Math.floor((this.products[i].price.amount * this.exchangeRates[j].rates[base]) * 100 ) / 100
+            break
+          }
+        }
+      }
+    }
+  }
 
+  handleEdit(item) {
+    let queryParams: NavigationExtras = {
+      queryParams: {
+        id: item.id,
+        base: this.base
+      }
+    }
+    this.router.navigate(['edit-product'], queryParams)
+  }
+
+  handleView(item) {
+    let queryParams: NavigationExtras = {
+      queryParams: {
+        id: item.id,
+        base: this.base
+      }
+    }
+    this.router.navigate(['view-product'], queryParams)
+  }
+
+  ngOnInit(): void {
+    this.products = JSON.parse(localStorage.getItem('products'))
+    this.exchangeRates = JSON.parse(localStorage.getItem('exchangeRates'))
+    this.route.queryParams.subscribe(params => {
+      if (params.base) {
+        this.base = params.base
+        this.switchBase(params.base)
+      }else {
+        this.base = 'AUD'
+      }
+    })
+    
+    if (!this.products || !this.exchangeRates) {
+      this.isModalShown = true
+      let timer = setInterval(() => {
+        this.products = JSON.parse(localStorage.getItem('products'))
+        this.exchangeRates = JSON.parse(localStorage.getItem('exchangeRates'))
+        if (this.products && this.exchangeRates) {
+          clearInterval(timer)
+          timer = null
+          this.isModalShown = false
+          this.switchBase(this.base)
+        }
+      }, 1000)
+    }else {
+      this.switchBase(this.base)
+    }
+  }
+  ngAfterViewInit() {
+    
+  }
 }
