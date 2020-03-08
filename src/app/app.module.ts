e16a43d2c812867dadb274ec2407f2aa45c1b907
ike <ikeikeike1024@gmail.com>
2020-03-08 10:40:20 +0800

    Initialize the read data logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,47 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { ViewProductComponent } from './components/view-product/view-product.component';
 
 
+let initData = () => {
+  return () => {
+    let readProductsData = () => {
+        let url = "assets/json/products.json";
+        let request = new XMLHttpRequest();
+        request.open("get", url);
+        request.send(null);
+        request.onload = function () {
+            if (request.status == 200) {
+                let json = JSON.parse(request.responseText);
+                localStorage.setItem('products', JSON.stringify(json))
+            }
+        }
+    }
+    let readExchangeRateData = () => {
+      let url = "assets/json/exchange_rates.json";
+      let request = new XMLHttpRequest();
+      request.open("get", url);
+      request.send(null);
+      request.onload = function () {
+          if (request.status == 200) {
+              let json = JSON.parse(request.responseText);
+              localStorage.setItem('exchangeRates', JSON.stringify(json))
+          }
+      }
+    }
+    let products:any = localStorage.getItem('products'),
+            exchangeRates:any = localStorage.getItem('exchangeRates')
+        if (!products) {
+            readProductsData()
+        }
+        if (!exchangeRates) {
+          readExchangeRateData()
+        }
+  }
+}
 
 @NgModule({
   declarations: [
